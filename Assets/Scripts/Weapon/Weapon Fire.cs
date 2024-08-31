effb12eec2214a529dd1608f5308307a6bd7d109
nerdPoint <thenerdpoint60@gmail.com>
2024-08-31 17:37:05 +0530

    [Added] Weapon 1 and Auto Firing Logic.

---
+++ Assets/Scripts/Weapon/Weapon Fire.cs
@@ -0,0 +1,75 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VampireSurvivor
+{
+    public class WeaponFire : MonoBehaviour
+    {
+        [SerializeField] private Transform firePoint;
+        [SerializeField] private WeaponStatsSO weaponStats;
+
+        private float nextFireTime = 0f;
+        private List<Collider2D> targetColliders = new();
+
+        private void Update()
+        {
+            if (Time.time >= nextFireTime && targetColliders.Count > 0)
+            {
+                Collider2D nearestTarget = FindNearestTarget();
+                if (nearestTarget != null)
+                {
+                    Fire(nearestTarget.transform.position);
+                    nextFireTime = Time.time + weaponStats.FireRate;
+                }
+            }
+        }
+
+        public void Fire(Vector2 targetPosition)
+        {
+            Vector2 direction = (targetPosition - (Vector2)firePoint.position).normalized;
+            GameObject projectile = PoolManager.Instance.GetFromPool(GamePoolType.Projectile);
+            projectile.transform.position = firePoint.position;
+            projectile.GetComponent<Projectile>().SetDirection(direction);
+        }
+
+        public void AddTarget(Collider2D target)
+        {
+            if (!targetColliders.Contains(target))
+            {
+                targetColliders.Add(target);
+            }
+        }
+
+        public void RemoveTarget(Collider2D target)
+        {
+            if (targetColliders.Contains(target))
+            {
+                targetColliders.Remove(target);
+            }
+        }
+
+        private Collider2D FindNearestTarget()
+        {
+            Collider2D nearestTarget = null;
+            float shortestDistance = Mathf.Infinity;
+
+            foreach (var target in targetColliders)
+            {
+                if (target == null)
+                {
+                    continue;
+                }
+
+                float distance = Vector2.Distance(firePoint.position, target.transform.position);
+                if (distance < shortestDistance)
+                {
+                    shortestDistance = distance;
+                    nearestTarget = target;
+                }
+            }
+
+            return nearestTarget;
+        }
+
+    }
+}
---
+++ Assets/Scripts/Weapon/WeaponStatsSO.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace VampireSurvivor
+{
+    [CreateAssetMenu(fileName = "WeaponStats", menuName = "ScriptableObjects/WeaponStats", order = 1)]
+    public class WeaponStatsSO : ScriptableObject
+    {
+        [SerializeField] private float fireRate = 1f;
+        [SerializeField] private float decreaseFireRateBy = 0.2f;
+
+        public float FireRate => fireRate;
+        public float DecreaseFireRateBy => decreaseFireRateBy;
+
+        public void DecreaseFireRate()
+        {
+            fireRate -= decreaseFireRateBy;
+        }
+    }
+}
