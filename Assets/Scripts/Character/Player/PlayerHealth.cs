7962e4ea8b48afd1cfa5fba2a32ee8cb26064670
nerdPoint <thenerdpoint60@gmail.com>
2024-09-02 20:33:23 +0530

    [Added] Player Hit Blood Particle.

--- Assets/Scripts/Character/Player/PlayerHealth.cs
+++ Assets/Scripts/Character/Player/PlayerHealth.cs
@@ -4,10 +4,13 @@ namespace VampireSurvivor
 {
     public class PlayerHealth : CharacterHealth
     {
+        [SerializeField] private ParticleSystem damageBloodParticle;
+
         public override void Damage(int damage)
         {
             base.Damage(damage);
             EventManager.TriggerEvent(GameEvents.PlayerHealth, (float)GetCurrentHealth);
+            damageBloodParticle.Play();
             if (HasDied())
                 EventManager.TriggerEvent(GameEvents.PlayerDie);
         }
